1466aaeb43ee2d4d8cc92d4437813d81acd902ec
jithin-rajesh <jithin.ayalloor@gmail.com>
2025-04-29 18:26:34 +0530

    Ignored stanford dataset training files

--- src/main/java/com/kitchen/model/InputIngredient.java
+++ src/main/java/com/kitchen/model/InputIngredient.java
@@ -3,31 +3,20 @@ package com.kitchen.model; // Using package from your screenshots
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * Represents a single ingredient item as found in the input JSON file.
- * Uses Jackson annotations to map JSON keys to Java fields.
- */
-// Allows Jackson to ignore any fields in the JSON that are not defined here
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class InputIngredient {
 
-    // Use Double to handle potential decimals like 1.5 cups
-    @JsonProperty("amount") // Maps JSON key "amount" to this field
+    @JsonProperty("amount")
     private Double amount;
 
-    @JsonProperty("unit") // Maps JSON key "unit" to this field
+    @JsonProperty("unit") 
     private String unit;
 
-    // Maps the "ingredient" field from JSON to this variable name
-    @JsonProperty("ingredient") // Maps JSON key "ingredient" to this field
+    @JsonProperty("ingredient")
     private String ingredientName;
 
-    // Default constructor (needed by Jackson, usually implicit)
     public InputIngredient() {
     }
-
-    // --- Getters and Setters ---
-    // Necessary for Jackson to serialize/deserialize and for other classes to access data
 
     public Double getAmount() {
         return amount;
@@ -53,7 +42,6 @@ public class InputIngredient {
         this.ingredientName = ingredientName;
     }
 
-    // Optional: toString() for debugging purposes
     @Override
     public String toString() {
         return "InputIngredient{" +
